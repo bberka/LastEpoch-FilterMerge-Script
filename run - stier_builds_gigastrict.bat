2690910662d16e7e46114cb567675c0ec5948c31
berg <berkaybayar@pm.me>
2026-04-04 14:59:42 +0300

    removed giga strict

--- run - stier_builds_gigastrict.bat
+++
@@ -1,2 +0,0 @@
-python merge_filters.py --builds ./stier_builds_gigastrict --core Core-Uber-Strict.xml --config config.yaml --output "TalaFilter CoF - All Maxroll S Tier Builds - Giga Strict.xml"
-@PAUSE

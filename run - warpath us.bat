80ae9f61f185ae835ec056e768425096ead0dff1
berg <berkaybayar@pm.me>
2026-04-04 17:22:39 +0300

    feat: warpath filters

---
+++ run - warpath us.bat
@@ -0,0 +1,2 @@
+python merge_filters.py --builds ./warpath-us --core Core-Uber-Strict.xml --config config.yaml --output "TalaFilter CoF - Warpath - Uber Strict.xml"
+@PAUSE
---
+++ run - warpath vs.bat
@@ -0,0 +1,2 @@
+python merge_filters.py --builds ./warpath-vs --core Core-Very-Strict.xml --config config.yaml --output "TalaFilter CoF - Warpath - Very Strict.xml"
+@PAUSE
